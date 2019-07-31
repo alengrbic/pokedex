354b654c4248a624b5a2019c9d4b97807a74a335
Alen Grbic <alengrbic90@gmail.com>
2019-07-31 15:04:00 +0100

    Updates

---
+++ precache-manifest.2c85749a979dcf3523fe300fae55e61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49bbb3652523dd7c8f685b0edb484c5",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "c4cf0b7b0865b1ec7483",
+    "url": "/pokedex/static/css/main.4af5db27.chunk.css"
+  },
+  {
+    "revision": "c11ff9c24bb617c910c1",
+    "url": "/pokedex/static/js/2.a49866a7.chunk.js"
+  },
+  {
+    "revision": "c4cf0b7b0865b1ec7483",
+    "url": "/pokedex/static/js/main.ccb3503f.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
