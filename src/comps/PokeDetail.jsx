f2f76890c10329afcbafa9f6f098efe9230b33c6
Alen Grbic <alengrbic90@gmail.com>
2019-07-31 18:51:01 +0100

    additional styling for pokemon types

--- src/comps/PokeDetail.jsx
+++ src/comps/PokeDetail.jsx
@@ -3,6 +3,47 @@ import "./PokeDetail.css";
 
 export class PokeDetail extends Component {
   render() {
+    //LONG FUNC FOR BACKGROUND COLOR
+    const backgroundClr = () => {
+      switch (this.props.types) {
+        case "electric":
+          return "#f8d030";
+        case "fire":
+          return "#f08030";
+        case "normal":
+          return "#a8a878";
+        case "fighting":
+          return "#c03028";
+        case "flying":
+          return "#a890f0";
+        case "poison":
+          return "#a040a0";
+        case "ground":
+          return "#e0c068";
+        case "rock":
+          return "#b8a038";
+        case "bug":
+          return "#a8b820";
+        case "ghost":
+          return "#705898";
+        case "steel":
+          return "#b8b8d0";
+        case "water":
+          return "#6890f0";
+        case "grass":
+          return "#78c850";
+        case "psychic":
+          return "#f85888";
+        case "ice":
+          return "#98d8d8";
+        case "dragon":
+          return "#7038f8";
+        case "dark":
+          return "#705848";
+        default:
+          return "white";
+      }
+    };
     return (
       <div>
         <div className="cont">
@@ -12,7 +53,9 @@ export class PokeDetail extends Component {
           <h2>
             {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)}
           </h2>
-          <h3>{this.props.types.toUpperCase()}</h3>
+          <h3 style={{ backgroundColor: backgroundClr() }}>
+            {this.props.types.toUpperCase()}
+          </h3>
 
           <h4>{this.props.text}</h4>
         </div>
